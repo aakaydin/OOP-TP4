10c33d3363f071e3041993a09680be8fc86e7e17
Oisín <oinolan@tcd.ie>
2020-01-23 10:42:42 +0100

    Tidied code and added graph output function

--- LogAnalyser.cpp
+++ LogAnalyser.cpp
@@ -5,52 +5,86 @@
 
 using namespace std;
 
-struct visits{
+struct visits {
 
     unordered_map<string,int>  visitors;
     int hits;
 
 };
 
+struct flagsData {
 
+    bool e;
+    bool g;
+    bool t;
 
+    int time;
+    string graphFilename;
 
+    flagsData() {
+        e = 0;
+        g = 0;
+        t = 0;
+        time = 0;
+    }
+};
 
+// Parses the arguments specified by the user at runtime
+flagsData parseArgs(int argc, char* argv[]);
+
+// Parses and returns the data from a log file in the logData data structure
+unordered_map<string,visits> parseFile(string filename, flagsData f);
+
+// For a given line, checks whether or not this data should be added to logData, for a given set of flags
+bool passFlagConditions(vector<string> parsedLine, flagsData f);
+
+// Adds the data from a given parsed line (vector<string>) to a given logData
+unordered_map<string,visits> addLineToLogData(unordered_map<string,visits>& logData, vector<string> input);
+
+// Prints the contents of 'logData'
+void printMapContents(unordered_map<string,visits> logData);
+
+void makeGraph(unordered_map<string,visits> logData, string filename);
 
 int main( int argc, char* argv[] )
 {
 
     string filename = argv[argc-1];
 
-    bool e = false;
-    bool t = false;
-    bool g = false;
+    flagsData f = parseArgs(argc, argv);
 
-    int time = 0;
-    string graphName = "";
+    unordered_map<string,visits> logData = parseFile(filename, f);   
+    
+    if(f.g) {
+        makeGraph(logData, f.graphFilename);
+    }
 
-    for( int i=0; i<argc;i++) cout << argv[i] << endl;
+}
+
+flagsData parseArgs(int argc, char* argv[]) {
+    flagsData f;
 
     for( int i=1; i<argc-1;i++ )
     {
         string s = argv[i];
 
-        if( s == "-e" ) e = true;
+        if( s == "-e" ) f.e = 1;
 
         if( s == "-t" )
         {
-            t = true;
-            time = stoi(argv[++i]);
+            f.t = 1;
+            f.time = stoi(argv[++i]);
         }
         if( s == "-g" )
         {
-            g = true;
-            graphName = argv[++i];
+            f.g = 1;
+            f.graphFilename = argv[++i];
         }
     }
+    return f;
+}
 
-
-
+unordered_map<string,visits> parseFile(string filename, flagsData f) {
     unordered_map<string,visits> logData;
 
     vector<string> input(9);
@@ -58,11 +92,8 @@ int main( int argc, char* argv[] )
     ifstream stream;
     stream.open(filename);
 
-
     while( !stream.eof() )
     {
-
-
         // IP
         getline(stream,input[0],' ');
         // User Logname
@@ -91,61 +122,130 @@ int main( int argc, char* argv[] )
         getline(stream,lol,'\n');
 
         if( stream.eof() ) break;
-        for( string s : input ) cout << s << " ";
-
-        cout << endl;
 
         // Filling logData
 
         // Date Flag
-        if(t)
+        if(!passFlagConditions(input, f)) {
+            continue;
+        }
+
+        // Populate logData with parsed line data
+        logData = addLineToLogData(logData, input);
+    }
+
+    return logData;
+}
+
+bool passFlagConditions(vector<string> parsedLine, flagsData f) {
+    if(f.t)
         {
             int hour;
-            for( int i=0;i<input[3].length; i++)
+            for( int i=0;i<parsedLine[3].length(); i++)
             {
-                if( input[3][i] == ':' )
+                if( parsedLine[3][i] == ':' )
                 {
-                    string h = input[3][i+1] +input[3][i+2];
+                    string h;
+                    h += parsedLine[3][i+1];
+                    h +=  + parsedLine[3][i+2];
                     hour = stoi(h);
                     break;
                 }
             }
-            if( hour != time ) continue;
+            return hour == f.time;
         }
 
-        // Log
+    // Handle remaining flags here
 
-        // string http = "http://intranet-if.insa-lyon.fr";
-        //
-        // for( int i=0;i<input[4].length; i++)
-        // {
-        //     if( input[4][i] == ' ')
-        //     {
-        //         i++;
-        //         while( input[4][i] != ' '  )
-        //         {
-        //             http += input[4][i];
-        //             i++;
-        //         }
-        //     }
-        // }
-        //
-        // string http2 = input[7];
-        //
-        // // not found
-        // if( logData.find(http) == logData.end() )
-        // {
-        //     visits v;
-        //     v.hits = 1;
-        //     v.visitors.insert("----",1);
-        //     logData.insert(make_pair())
-        // }
-        // else // found
-        // {
-        //
-        // }
+    return true;
+}
 
+unordered_map<string,visits> addLineToLogData(unordered_map<string,visits>& logData, vector<string> input) {
+    string http = "http://intranet-if.insa-lyon.fr";
+        
+    for( int i=0;i<input[4].length(); i++)
+    {
+        if( input[4][i] == ' ')
+        {
+            i++;
+            while( input[4][i] != ' '  )
+            {
+                http += input[4][i];
+                i++;
+            }
+        }
+    }
+    
+    string http2 = input[7];
+    
+    // not found
+    if( logData.find(http) == logData.end() )
+    {
+        visits newVisits;
+        newVisits.hits = 1;
+        newVisits.visitors.insert(make_pair(http2, 1));
+        logData.insert(make_pair(http, newVisits));
+    }
+    else // found
+    {
+
+        visits keyData = logData.at(http);
+        keyData.hits++;
+        if(keyData.visitors.find(http2) == keyData.visitors.end()) {
+            keyData.visitors.insert(make_pair(http2, 1));
+        } else {
+            keyData.visitors.at(http2)++;
+        }
+        logData[http] = keyData;
     }
 
+    return logData;
+}
+
+void printMapContents(unordered_map<string,visits> logData) {
+    for(auto& p: logData) {
+        std::cout << " " << p.first << " -- hits: " << p.second.hits << "\n" ;
+        for(auto& q: p.second.visitors) {
+            cout << "   " << q.first << " - " << q.second << endl;
+        }
+    }
+}
+
+void makeGraph(unordered_map<string,visits> logData, string filename) {
+    
+    string nodes;
+    string edges;
+
+    // nodeNames stores a mapping between generated nodeX variable names
+    // and their identifying URLs, with n being the total number of distinct nodes.
+    unordered_map<string, string> nodeNames;
+    int n = 0;
+
+    for(auto& data: logData) {
+
+        string nodeName = "node" + to_string(n++);
+        nodes += nodeName + " [label=\"" + data.first + "\"];\n";
+        nodeNames.insert(make_pair(data.first, nodeName));
+
+        for(auto& visitor : data.second.visitors) {
+
+            string visitorNodeName;
+            if(nodeNames.find(visitor.first) == nodeNames.end()) {
+                visitorNodeName = "node" + to_string(n++);
+                nodes += visitorNodeName + " [label=\"" + visitor.first + "\"];\n";
+                nodeNames.insert(make_pair(visitor.first, visitorNodeName));
+            } else {
+                visitorNodeName = nodeNames.at(visitor.first);
+            }
+
+            edges += visitorNodeName + " -> " + nodeName + "[label=\"" + to_string(visitor.second) + "\"];\n";
+        }
+    }
+
+    string fileText = "digraph {\n" + nodes + edges + "}";
+
+    ofstream out(filename);
+    out << fileText;
+    out.close();
 
 }
